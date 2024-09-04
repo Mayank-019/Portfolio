ffc76ed583043a6c47ca93a08264572d80be64ea
Mayank Kanojia <113011870+Mayank-019@users.noreply.github.com>
2024-09-04 19:44:54 +0530

    Add files via upload

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     let lastScrollTop = 0;
-//     const navbar = document.querySelector('nav');
-
-//     window.addEventListener('scroll', () => {
-//         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-//         if (scrollTop > lastScrollTop) {
-//             // Scrolling down
-//             navbar.classList.add('nav-hidden');
-//             navbar.classList.remove('nav-visible');
-//         } else {
-//             // Scrolling up
-//             navbar.classList.add('nav-visible');
-//             navbar.classList.remove('nav-hidden');
-//         }
-
-//         lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-//     });
-// });
 
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('nav');
